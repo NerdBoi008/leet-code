f5fb170d9d714a8dadf76c9619b276bbc957fe99
nerdboi008 <mail.nerdboi@gmail.com>
2026-01-03 20:40:54 +0530

    Solves 0013-roman-to-integer.ts question

---
+++ easy/0013-roman-to-integer.ts
@@ -0,0 +1,41 @@
+/**
+ * LeetCode #1 - Two Sum
+ * Difficulty: Easy
+ * https://leetcode.com/problems/roman-to-integer
+ *
+ * Time: O(n)
+ * Space: O(1)
+ */
+
+function romanToInt(s: string) {
+  const romanMap: Map<string, number> = new Map(
+    Object.entries({
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000,
+    })
+  );
+
+  let total = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const current = romanMap.get(s[i])!;
+    const next = romanMap.get(s[i + 1])!;
+
+    if (current < next) {
+      total -= current;
+    } else {
+      total += current;
+    }
+  }
+
+  return total;
+}
+
+console.log(romanToInt("III"));
+console.log(romanToInt("LVIII"));
+console.log(romanToInt("MCMXCIV"));
--- easy/README.md
+++ easy/README.md
@@ -4,3 +4,4 @@
 |---|------|---------|
 | 1 | Two Sum | [0001-two-sum.ts](./0001-two-sum.ts) |
 | 9 | Palindrome Number | [0009-palindrome-number.ts](./0009-palindrome-number.ts) |
+| 13 | Roman To Integer | [0013-roman-to-integer.ts](./0013-roman-to-integer.ts) |
